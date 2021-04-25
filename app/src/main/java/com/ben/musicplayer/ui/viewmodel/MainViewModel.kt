c4d13701de1b4839b14eee6944a11d176a7038a3
BenjaminSpoiden <spoiden.benjamin@hotmail.com>
2021-04-25 18:28:25 +0200

    finished up main view model

--- app/src/main/java/com/ben/musicplayer/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/ben/musicplayer/ui/viewmodel/MainViewModel.kt
@@ -1,6 +1,7 @@
 package com.ben.musicplayer.ui.viewmodel
 
 import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaMetadataCompat.METADATA_KEY_MEDIA_ID
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -8,6 +9,9 @@ import com.ben.musicplayer.model.Song
 import com.ben.musicplayer.network.ResponseStatus
 import com.ben.musicplayer.service.MusicServiceConnection
 import com.ben.musicplayer.utils.MEDIA_ROOT_ID
+import com.ben.musicplayer.utils.isPlayEnabled
+import com.ben.musicplayer.utils.isPlaying
+import com.ben.musicplayer.utils.isPrepared
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -58,13 +62,23 @@ class MainViewModel @Inject constructor(
         musicServiceConnection.transportControls.seekTo(pos)
     }
 
-    fun playOrToggleSong() {
-
+    fun playOrToggleSong(song: Song, toggle: Boolean = false) {
+        val isPlayerPrepared = playbackState.value?.isPrepared ?: false
+        if(isPlayerPrepared && song.mediaId == currentlyPlayingSong.value?.getString(METADATA_KEY_MEDIA_ID)) {
+            playbackState.value?.let { playbackStateCompat ->
+                when {
+                    playbackStateCompat.isPlaying -> if(toggle) musicServiceConnection.transportControls.pause()
+                    playbackStateCompat.isPlayEnabled -> musicServiceConnection.transportControls.play()
+                    else -> Unit
+                }
+            }
+        } else {
+            musicServiceConnection.transportControls.playFromMediaId(song.mediaId, null)
+        }
     }
 
     override fun onCleared() {
         super.onCleared()
         musicServiceConnection.onUnsubscribe(MEDIA_ROOT_ID, object : MediaBrowserCompat.SubscriptionCallback() {})
     }
-
 }
